c921f2ea919c89d6642f59392af917f61126c6f0
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-04-26 23:07:49 -0300

    cambios

---
+++ Set-map/app.js
@@ -0,0 +1,4 @@
+/* ejercicios
+* 
+*/
+console.log('zona de problemas con Set/Map')
--- Set-map/script.js
+++ Set-map/script.js
@@ -12,12 +12,17 @@ set.add(null)
 set.add(NaN);
 set.add("A");
 set.add("pepe");
-
-set;            // Set({5, "A"})
+set;
 console.log(set);
-const set1 = new Set([8,5,6,7,8]);
+console.log('probando propiedad .size')            // Set({5, "A",NaN,8,undefined,null,pepe})
+const set1 = new Set([5,6,7,8,9,11,13]);
 set1;
-console.log(set1.size)//4
+console.log(set1.size)//7
+console.log('probando propiedad .has')
+console.log(set1.has(4))//false
+console.log(set1.has(8))//true
+console.log(set1.has(12))//false
+console.log(set1.has(13))//true
 
 
 
