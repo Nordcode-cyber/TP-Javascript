faf47ea1886310f89af4bc66676c20b63befd6f6
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-04-29 21:24:54 -0300

    news files html

--- igualdad/script.js
+++ igualdad/script.js
@@ -1,6 +1,8 @@
 let a = 3, b = 5, c = 3, d = 5, e = '5';
 let z = a == c; // se revisa el valor sin importar el tipo
-console.log(z); //true
+console.log('igualdad en JS')
+console.log(typeof z); //true
+console.log( z); //true
 z = a==d;
 console.log(z);
 z = b ==d;
