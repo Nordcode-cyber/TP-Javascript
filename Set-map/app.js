5e82c6caabd091301558c8fafcd593e2f881aff5
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-05-03 22:09:41 -0300

    modified files

--- Set-map/app.js
+++ Set-map/app.js
@@ -1,12 +1,13 @@
 let map = new Map();
 
-map.set('1', 'str1');   // un string como clave
-map.set(1, 'num1');     // un número como clave
-map.set(true, 'bool1'); // un booleano como clave
+map.set('1', 'string');   // un string como clave
+map.set(1, 'number');     // un número como clave
+map.set(true, 'boolean'); // un booleano como clave
 
 // ¿recuerda el objeto regular? convertiría las claves a string.
 // Map mantiene el tipo de dato en las claves, por lo que estas dos son diferentes:
 console.log( map.get(1)   ); // 'num1'
 console.log( map.get('1') ); // 'str1'
+console.log(map.get(true))
 
 console.log( map.size ); // 3
