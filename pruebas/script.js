f524c18ae0d6a0328a0dca91c74c950277e22c74
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-05-07 21:09:31 -0300

    test

---
+++ pruebas/script.js
@@ -0,0 +1,16 @@
+//Declaración de la función
+function miFuncion1(){
+    console.log('funcion 1');
+    
+}
+function miFuncion2(){
+    console.log('funcion 2');
+    
+}
+//arguments.legth devuelve la cantidad de parametros de una funcion
+//Llamando a la función
+miFuncion2();
+miFuncion1();
+
+//console.log(f1)
+//console.log(f2)
