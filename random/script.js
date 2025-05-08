d271b87d5f13bfb25bc76adb19edf9e880f6f832
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-05-07 22:48:15 -0300

    parsing ramdom

---
+++ random/script.js
@@ -0,0 +1,6 @@
+function random(min, max) {
+    return min + Math.random() * (max - min);
+  }
+console.log('un numero random entre 4 y 5: '+' '+random (4,5));
+console.log('un numero random entre 10 y 99: '+' '+random (10,99));
+console.log('un numero random entre 100 y 120: '+' '+random (100,120));
