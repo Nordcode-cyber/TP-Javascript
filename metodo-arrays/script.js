89c6031e494083d0e9ecf9cd644f7b655fd45d5d
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-05-09 22:51:10 -0300

    methods arrays javascript

--- metodo-arrays/script.js
+++ metodo-arrays/script.js
@@ -32,11 +32,18 @@ console.log( arr5.slice(1, 3) ); // e,s (copia desde 1 hasta 3)
 
 console.log( arr5.slice(0) ); // s,t (copia desde -2 hasta el final)
 let arr6=['yo','estudio','typescript','para','saber','Angular'];
-console.table(arr6)
 console.log('tamaño del array arr6: '+arr6.length)
 console.log('funcion ForEach javascript')
+console.table(arr6)
 arr6.forEach(function(arr6, index) {
   console.log('indice: '+ index+ " " + 'valor: '+ arr6);// ... hacer algo con un elemento
 });
+let arr7 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+arr7.forEach(function(arr7, index) {
+  console.log('indice: '+ index+ " " + 'valor: '+ arr7);// ... hacer algo con un elemento
+});
+console.log('metodo concat')
+let arr8 =[3,4,5]
+console.log( arr8.concat([6, 7]) );
 
 
